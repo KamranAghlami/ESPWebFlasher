2511c26d2611edf2b7fc14e6d15670abf378ce9a
Kamran <oops.legendary@gmail.com>
2024-06-01 17:09:04 +0300

    intial

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,53 @@ import './css/style.css';
 window.addEventListener('DOMContentLoaded', () => {
     import('./terminal').then(({ initiate_termial }) => {
         initiate_termial();
+
+        main();
     });
 });
+
+interface Release {
+    tag_name: string;
+    assets: Asset[];
+}
+
+interface Asset {
+    name: string;
+    browser_download_url: string;
+}
+
+function get_repository_url(): string | null {
+    const params = new URLSearchParams(window.location.search);
+
+    return params.get('repo');
+}
+
+async function main() {
+    const repository_url = get_repository_url();
+
+    if (!repository_url) {
+        console.error('Error: Repository URL not specified');
+
+        return;
+    }
+
+    const response = await fetch(`https://api.github.com/repos/${repository_url}/releases`);
+
+    if (!response.ok) {
+        console.error('Failed to fetch releases');
+        return;
+    }
+
+    const releases: Release[] = await response.json();
+
+    if (releases.length)
+        console.log(`\n  Releases:`);
+
+    releases.forEach(release => {
+        console.log(`   * ${release.tag_name}`);
+
+        release.assets.forEach(asset => {
+            console.log(`     - \u001b]8;;${asset.browser_download_url}\u0007${asset.name}\u001b]8;;\u0007`);
+        });
+    });
+}
